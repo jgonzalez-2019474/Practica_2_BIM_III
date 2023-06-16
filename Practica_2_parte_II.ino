aec4522cda6f610ceaefa1fa147ffdbd167c896a
jgonzalez-2019474 <129011598+jgonzalez-2019474@users.noreply.github.com>
2023-06-16 08:30:57 -0600

    Update Practica_2_parte_II.ino

--- Practica_2_parte_II.ino
+++ Practica_2_parte_II.ino
@@ -7,7 +7,7 @@
  * Curso: Taller de electronica digital y reparación de computadoras I
  * Nombre: Javier Andre Gonzalez Barrera
  * Carnet: 2019474
- * Proyecto: Practica #3
+ * Proyecto: Practica #2
 */
 #include <Ticker.h>
 
